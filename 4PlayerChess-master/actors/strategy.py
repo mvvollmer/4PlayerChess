7d20a38d55ca145bff851dcbc24020d249aee483
Andrew Yuan <andrewwyuan@gmail.com>
2022-11-16 11:00:04 -0500

    added functions

--- 4PlayerChess-master/actors/strategy.py
+++ 4PlayerChess-master/actors/strategy.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append('./4PlayerChess-master/')
 from gui.board import Board
+from gui.boardStruct import BoardStruct
+
+RED, BLUE, YELLOW, GREEN, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING = range(10)
 
 class Strategy():
   def __init__(self, player: str):
@@ -16,7 +19,7 @@ class Strategy():
     # promote space is the file, rank of the piece being promoted
     pass
 
-  def getLegalMoves(self, board: Board, piece: int, fromFile: int, fromRank: int, player: str):
+  def getLegalMoves(self, board: Board, piece: int, fromFile: int, fromRank: int):
     # get legal moves for a piece given the board state. Taken from view function.
     # TODO: check that when en-passant and checks/checkmates are working that the board functions used here
     #       still work
@@ -30,7 +33,7 @@ class Strategy():
     # get the pieces which can move for this player
     moveable_pieces = []
     for i, space in enumerate(board.boardData):
-      if space[0] == self.player:
+      if space[0] == player:
         file, rank = board.indexToFileRank(i)
         piece = board.getPiece(space)
         moves, captures = self.getLegalMoves(board, piece, file, rank)
@@ -38,5 +41,19 @@ class Strategy():
           moveable_pieces.append((space, file, rank))
     return moveable_pieces # list of tuples. ex tuple: ('rP', 3, 2)
 
-  def getNewBoard(self, board: Board, boardData: list):
-    pass
+  def getNewBoard(self, board: Board, fromFile, fromRank, toFile, toRank):
+    """
+    Given a board and a new board state, return a new board struct that contains the new board state.
+    Note: this board cannot be used to make moves on the actual board, it is only meant to be used with
+    search algorithms
+    """
+    newBoard = BoardStruct(14, 14)
+    newBoard.boardData = board.boardData.copy()
+    newBoard.pieceBB = board.pieceBB.copy()
+    newBoard.occupiedBB = board.pieceBB[RED] | board.pieceBB[BLUE] | board.pieceBB[YELLOW] | board.pieceBB[GREEN]
+    newBoard.emptyBB = ~newBoard.occupiedBB
+    newBoard.enPassant = board.enPassant.copy()
+    newBoard.castle = board.castle.copy()
+    newBoard.makeMove(fromFile, fromRank, toFile, toRank)
+    return newBoard
+
